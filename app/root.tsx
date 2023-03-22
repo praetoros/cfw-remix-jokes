a5534ba6c1b34699e7f6815baa0a0d1175f84ee0
Tom Alle <development@praetoros.com>
2023-03-23 01:23:08 +1100

    -> styling

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import {LiveReload} from "@remix-run/react";
+import {Links,LiveReload, Outlet} from "@remix-run/react";
 
 export default function App() {
     return (
@@ -6,9 +6,10 @@ export default function App() {
         <head>
             <meta charSet="utf-8"/>
             <title>Remix: So great, it's funny!</title>
+            <Links />
         </head>
         <body>
-        Hello world
+        <Outlet/>
         <LiveReload/>
         </body>
         </html>
